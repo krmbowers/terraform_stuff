00d8437a319a8b9add978d56794373d8dc2ef68b
kristin.bowers <krmbowers@gmail.com>
2018-11-14 21:34:54 +0000

    commit

---
+++ vm.tf
@@ -0,0 +1,25 @@
+resource "azurerm_virtual_network" "mynet" {
+  name="myvnet"
+  address_space = ["10.0.0.0/16"]
+  location= "${var.location}"
+  resource_group_name="${azurerm_resource_group.thegroup.name}"
+  tags {
+    environment = "testing"
+  }
+}
+resource "azurerm_subnet" "mysub"{
+  name="mysubnet"
+  resource_group_name="${azurerm_resource_group.thegroup.name}"
+  virtual_network_name="${azurerm_virtual_network.mynet.name}"
+  address_prefix="10.0.1.0/24"
+}
+
+resource "azurerm_public_ip" "myip" {
+  name="table2kbpubip"
+  location="${var.location}"
+  resource_group_name="${azurerm_resource_group.thegroup.name}"
+  public_ip_address_allocation="dynamic"
+  tags {
+    environment = "testing"
+  }
+}
